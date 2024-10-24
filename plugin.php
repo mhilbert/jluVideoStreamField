cd9e038aa07f18653064d18ed78d1427059905cd
Mirco Hilbert <mhilbert@users.noreply.github.com>
2024-10-24 09:49:24 +0200

    Increase version for ILIAS 9

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'xjludclvs';
-$version = '1.1.0';
+$version = '1.2.0';
 $ilias_min_version = '8.0';
-$ilias_max_version = '8.999';
+$ilias_max_version = '9.999';
 $responsible = 'Mirco Hilbert';
 $responsible_mail = 'Mirco.Hilbert@hrz.uni-giessen.de';
 ?>
